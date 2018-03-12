5657dc5530c157677ee673a9b385809b4edb3b18
Cloud User <centos@ip-172-31-34-145.us-east-2.compute.internal>
2018-03-12 19:41:00 +0000

    initial workstation cookbook commit

---
+++ recipes/setup.rb
@@ -0,0 +1,21 @@
+package 'tree' do
+  action :install
+end
+
+package 'ntp' do
+  action :install
+end
+
+package 'git' do
+  action :install
+end
+
+file '/etc/motd' do
+  content 'Property of Santiago Gallo'
+  owner 'root'
+  group 'root'
+end
+
+service 'ntpd' do
+  action [ :enable, :start ]
+end
