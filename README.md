24a36d040d3ea09e066c6001ddcca4d16898bed1
Cloud User <centos@ip-172-31-34-145.us-east-2.compute.internal>
2018-03-15 19:59:38 +0000

    Modifique setup.rb con node attributes y el metadata v0.3.0

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # este es el workstation recipe
 
-TODO: Enter the cookbook description here.
+TODO: Enter the cookbook description here. Modifique metada v 0.3.0
 
--- recipes/setup.rb
+++ recipes/setup.rb
@@ -12,10 +12,18 @@ package 'git' do
   action :install
 end
 
+#
+# prit statement 'I have 4 apples'
+# apple_count = 4
+# puts "I have #{apple_count} apples"
+
+#  MEMORY: #{node['memory']['total']}
 file '/etc/motd' do
-  content 'Property of Santiago Gallo'
-  owner 'root'
-  group 'root'
+  content "Property of Santiago Gallo
+  HOSTNAME: #{node['hostname']}
+  IPADDRESS: #{node['ipaddress']} 
+  CPU: #{node['cpu']['0']['mhz']}
+" 
 end
 
 service 'ntpd' do
