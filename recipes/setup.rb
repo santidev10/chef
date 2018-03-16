b59ea9bccb232d0e0d837e566447c18794a70705
Cloud User <centos@ip-172-31-34-145.us-east-2.compute.internal>
2018-03-16 20:15:18 +0000

    refactored motd file with a template

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -12,18 +12,9 @@ package 'git' do
   action :install
 end
 
-#
-# prit statement 'I have 4 apples'
-# apple_count = 4
-# puts "I have #{apple_count} apples"
-
-#  MEMORY: #{node['memory']['total']}
-file '/etc/motd' do
-  content "Property of Santiago Gallo
-  HOSTNAME: #{node['hostname']}
-  IPADDRESS: #{node['ipaddress']} 
-  CPU: #{node['cpu']['0']['mhz']}
-" 
+template '/etc/motd' do
+  source 'motd.erb'
+  action :create
 end
 
 service 'ntpd' do
